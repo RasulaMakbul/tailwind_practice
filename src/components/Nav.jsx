a6115987a5f61ea8ed8c92a651a5756c2599044a
RasulaMakbul <emu.cfs@gmail.com>
2024-05-14 18:48:52 +0600

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
+import {hamburger} from "../assets/icons"
 import {headerLogo} from "../assets/images"
+import {navLinks} from "../constants"
 
 const Nav = () => {
   return (
@@ -7,7 +9,21 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden"></ul>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map(item => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,16 @@
 const Hero = () => {
-  return <div>Test Hero</div>
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col
+       justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-ful max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>The New Arrival</h1>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
